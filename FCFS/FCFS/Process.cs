c6e0ccf28e8d5698f6e3672d2a288b6dbfe44f70
Pumelela-Banca <pumelelabanca@gmail.com>
2026-04-05 14:23:39 +0200

    added Process class

---
+++ FCFS/FCFS/Process.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FCFS
+{
+    internal class Process
+    {
+        public string Id { get; set; }
+        public int Size { get; set; }
+    }
+}
